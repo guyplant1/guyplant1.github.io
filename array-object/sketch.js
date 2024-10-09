3989d4bd71b0936db61a5ce5dad1a72d3fa84835
guyplant1 <guyplant1@hotmail.com>
2024-10-09 11:10:57 -0600

    Updated my Arrays and Object Notation Assignment: In progress for centering characters (represented as blue circles) in the first top graph blocks.
    Also wanting to start working on clicking on the character and placing it on the green parts of the graph.

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -29,24 +29,30 @@ function setup() {
 function draw() {
   background(220);
   
+  console.log(window.innerWidth);
+
+  //let characterXChange = cellSize/2;
   for(let y = 0; y < 8; y++) {
     for(let x = 0; x < 16; x++) {
       if (y === 0) {
         fill("orange");
         stroke("white");
         square(x * cellSize, y * cellSize, cellSize);
-        // last here with circle
+        // last here with circle -added: figuring out centering of circle characters  --- //--characterXChange * x + 50
         fill("blue");
-        circle(cellSize/2, cellSize/2, 20);
+        circle(x * cellSize + 50, cellSize/2, 20);
+        //characterXChange = cellSize;
       }
       else {
         fill("green");
+        stroke("black");
         square(x * cellSize, y * cellSize, cellSize);
       }
     }
   }
 }
 
-function characterPlacement() {
+// function characterPlacement() {
 
-}
+// }
+
