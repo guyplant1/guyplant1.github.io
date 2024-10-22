897fbef1cfdfa6468cb43ab33d631c612ec0f221
guyplant1 <guyplant1@hotmail.com>
2024-10-22 06:09:53 -0600

    I feel that I did a lot of experimenting of how to fix the beverage character colors, and seeing how to interact with the created cells on the game board of my project. I'm not completely done, but my goal is to at least have the interaction between the idea of arrayed opponent blocks and the user's placement of the beverage characters be possible.
    I feel that I want to go in the process of commenting all of my current code before working again on this assignment, for the outcome from the rubric, and to then start into the logic of how to put the beverage characters onto the game board by a user's placement/click.

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,7 +5,18 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+// Steps remaining: 1. Able to click on beverage/beverage cell to then put the selected beverage onto the game board.
+// Maybe if the mouse is clicked again, but done somewhere else besides the game board, then it'll cancel the process of placing a beverage down.
+// 2. Make another array that displays blocks coming from the right side of the screen/window, placed in one of the cell sections on the game board.
+// Then it'll keep on moving until it reaches the very left of the screen/window. I'm thinking to use text numbers displayed on the blocks to show their difficulty.
+// 3. Make the beverages shoot from themselves circles at the blocks, making the blocks visually increase in color and the beverages decrease in color.
+// 4. Extra for Experts: I'm thinking to look a little bit into visuals to experiment with, maybe shapes and or other things able I'm able to display in this assignment.
+// 5. This is for if I'm able to add a start screen and end screen for the user's game experience.
+
 let beverageCharacters = [];
+let beverageCells = [];
+let gameBoardCells = [];
 let cellSize;
 let selectedBeverage = "water";
 let beverageX;
@@ -25,6 +36,8 @@ function setup() {
   //   beverageDraw();
   // }
 
+  graphDraw();
+
 }
 
 
@@ -35,7 +48,7 @@ function draw() {
 
   //console.log(window.innerWidth);
 
-  graphDraw();
+  displayGraphCells();
 
   //beveragePositionDraw();
 
@@ -48,42 +61,54 @@ function graphDraw() {
   for(let y = 0; y < 8; y++) {
     for(let x = 0; x < 16; x++) {
       if (y === 0) {
-        fill("orange");
-        stroke("white");
-        square(x * cellSize, y * cellSize, cellSize);
-        
-        if (x < 7) {
+        beverageCellDraw(x * cellSize, y * cellSize, cellSize);
+        // fill("orange");
+        // stroke("white");
+        // square(x * cellSize, y * cellSize, cellSize);
+        if (x < 6) {
           beverageDraw(x);
         }
 
-        //anotherDisplayBeverage(x);
-
-        // beverageDraw(x);
-        // anotherDisplayBeverage(x);
-        
-
-
-        //beverageColor(x);
-        //displayBeverageSelection(x, y);
-
       }
       else {
-        fill("green");
-        stroke("black");
-        square(x * cellSize, y * cellSize, cellSize);
+        gameBoardCellDraw(x * cellSize, y * cellSize, cellSize);
+        // fill("green");
+        // stroke("black");
+        // square(x * cellSize, y * cellSize, cellSize);
       }
     }
   }
 }
 
 
-// function beveragePositionDraw() {
-//   for (let y = 0; y < 8; y++) {
-//     for(let x = 0; x < 16; x++) {
-//       if (y === 0 && x < 7) {
-//         beverageDraw(x);
-//       }
-//     }
+function displayGraphCells() {
+  //let button = false;
+  for (let cell of beverageCells) {
+    if (mouseX > cell.beverageCellX && mouseX < cell.beverageCellX + cell.beverageCellSize && mouseY > cell.beverageCellY && mouseY < cell.beverageCellY + cell.beverageCellSize) {
+      stroke("orange");
+      fill("yellow");
+    }
+    else if (mouseX > cell.beverageCellX && mouseX < cell.beverageCellX + cell.beverageCellSize && mouseY > cell.beverageCellY && mouseY < cell.beverageCellY + cell.beverageCellSize && mouseIsPressed) {
+      fill("green");
+    }
+    else {
+      fill("orange");
+      stroke("white");
+    }
+    square(cell.beverageCellX, cell.beverageCellY, cell.beverageCellSize);
+  }
+
+  fill("green");
+  stroke("black");
+  for (let boardCell of gameBoardCells) {
+    square(boardCell.gameBoardCellX, boardCell.gameBoardCellY, boardCell.gameBoardCellSize);
+  }
+}
+
+
+// function mousePressed() {
+//   if (mouseX > cell.beverageCellX && mouseX < cell.beverageCellX + cell.beverageCellSize && mouseY > cell.beverageCellY && mouseY < cell.beverageCellY + cell.beverageCellSize) {
+//     button = !button;
 //   }
 // }
 
@@ -101,55 +126,204 @@ function beverageDraw(x) {
 
 
 function anotherDisplayBeverage() { // x was in here
-  let x = 0;
+  let beverageState = "water";
+  stroke("white");
   for (let beverage of beverageCharacters) {
-    fill(beverageColor(x));
-    rect(beverage.cupX, beverage.cupY, beverage.cupW, beverage.cupLiquidH);
-    fill(160);
-    rect(beverage.cupX, beverage.cupY, beverage.cupW, beverage.cupEmptyH);
-    x += 1;
+    if (beverageState === "water") {
+      fill(0, 150, 255);
+      buildBeverage(beverage);
+      beverageState = "chocolate milk";
+    }
+  
+    else if (beverageState === "chocolate milk") {
+      fill(120, 40, 0);
+      buildBeverage(beverage);
+      beverageState = "lemonade";
+    }
+    
+    else if (beverageState === "lemonade") {
+      fill(0, 0, 0);
+      buildBeverage(beverage);
+      beverageState = "ice tea";
+    }
+    
+    else if (beverageState === "ice tea") {
+      fill(180, 50, 0);
+      buildBeverage(beverage);
+      beverageState = "coffee";
+    }
+    
+    else if (beverageState === "coffee") {
+      fill(50, 0, 0);
+      buildBeverage(beverage);
+      beverageState = "orange juice";
+    }
+    
+    else if (beverageState === "orange juice") {
+      fill(250, 120, 0);
+      buildBeverage(beverage);
+      beverageState = "water";
+    }
   }
 }
 
 // x * beverage.cupX replaced for x * cellsize + 30
 
 
-function beverageColor(x) {
-  let color;
-  if (x === 0) {
-    color = [0, 150, 255];
-    //selectedBeverage = "chocolate milk";
-  }
-
-  else if (x === 1) {
-    //selectedBeverage = "lemonade";
-    color = [120, 40, 0];
-  }
-  
-  else if (x === 2) {
-    //selectedBeverage = "ice tea";
-    color = [0, 0, 0];
-  }
-  
-  else if (x === 3) {
-    //selectedBeverage = "coffee";
-    color = [180, 50, 0];
-  }
-  
-  else if (x === 4) {
-    //selectedBeverage = "orange juice";
-    color = [50, 0, 0];
-  }
-  
-  else if (x === 5) {
-    //selectedBeverage = "water";
-    color = [250, 120, 0];
-  }
-  return color;
+function buildBeverage(beverage) {
+  rect(beverage.cupX, beverage.cupY, beverage.cupW, beverage.cupLiquidH);
+  fill(160);
+  rect(beverage.cupX, beverage.cupY, beverage.cupW, beverage.cupEmptyH);
 }
 
 
+// function beverageColor(x) {
+//   let color;
+//   if (x === 0) {
+//     color = [0, 150, 255];
+//     //selectedBeverage = "chocolate milk";
+//   }
 
+//   else if (x === 1) {
+//     //selectedBeverage = "lemonade";
+//     color = [120, 40, 0];
+//   }
+  
+//   else if (x === 2) {
+//     //selectedBeverage = "ice tea";
+//     color = [0, 0, 0];
+//   }
+  
+//   else if (x === 3) {
+//     //selectedBeverage = "coffee";
+//     color = [180, 50, 0];
+//   }
+  
+//   else if (x === 4) {
+//     //selectedBeverage = "orange juice";
+//     color = [50, 0, 0];
+//   }
+  
+//   else if (x === 5) {
+//     //selectedBeverage = "water";
+//     color = [250, 120, 0];
+//   }
+//   return color;
+// }
+
+
+// function beverageColorState() {
+//   if (x === 0) {
+//     fill(0, 150, 255);
+//   }
+
+//   else if (x === 1) {
+//     fill(120, 40, 0);
+//   }
+  
+//   else if (x === 2) {
+//     fill(0, 0, 0);
+//   }
+  
+//   else if (x === 3) {
+//     fill(180, 50, 0);
+//   }
+  
+//   else if (x === 4) {
+//     fill(50, 0, 0);
+//   }
+  
+//   else if (x === 5) {
+//     fill(250, 120, 0);
+//   }
+// }
+
+
+// function mousePressed() {
+//   for (let beverage of beverageCharacters) {
+//     if (beverageIsClicked(mouseX, mouseY, beverage)) {
+//       // beverage.cupX = mouseX - beverage.cupW/2;
+//       // beverage.cupY = mouseY - beverage.cupLiquidH/2;
+//       for (let cell of beverageCells) {
+//         cell.beverageCellStateColor = "white";
+//         // if (beverageAndCellAligned(beverage, cell)) {
+//         //   //console.log("true");
+//         //   cell.beverageCellStateColor = "white";
+//         // }
+//         // else {
+//         //   //console.log("false");
+//         // }
+//       }
+
+
+//       // beverage.cupX = mouseX - beverage.cupW/2;
+//       // beverage.cupY = mouseY - beverage.cupLiquidH/2;
+//     }
+//   }
+
+  
+//   // if (mouseX > beverageX && mouseX < beverageX + w && mouseY > beverageY && mouseY < beverageY + h) {
+//   //   beverageX = mouseX - w/2;
+//   //   beverageY = mouseY - h/2;
+//   // }
+// }
+
+
+// function beverageIsClicked(x, y, beverage) {
+//   let distanceAway = dist(x, y, beverage.cupX, beverage.cupY);
+//   if (distanceAway < beverage.cupW) {
+//     //console.log("true");
+//     return true;
+//   }
+//   else {
+//     //console.log("false");
+//     return false;
+//   }
+// }
+
+
+function gameBoardCellDraw(x, y, size) {
+  let gameBoardCellDrawn = {
+    gameBoardCellX: x,
+    gameBoardCellY: y,
+    gameBoardCellSize: size,
+    gameBoardCellColor: "green"
+  };
+  gameBoardCells.push(gameBoardCellDrawn);
+}
+
+
+function beverageCellDraw(x, y, size) {
+  let beverageCellDrawn = {
+    beverageCellX: x,
+    beverageCellY: y,
+    beverageCellSize: size,
+    beverageCellStateColor: "orange",
+  };
+  beverageCells.push(beverageCellDrawn);
+  //console.log("beverage cells");
+}
+
+
+// function beverageAndCellAligned(beverage, cell) {
+//   //let cellAndBeverageAlignment = dist(beverage.cupX, beverage.cupY, cell.beverageCellX, cell.beverageCellY);
+//   if (beverage.cupX - 30 === cell.beverageCellX) { //cellAndBeverageAlignment > cell.beverageCellSize
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+
+// function beverageCellColorState() {
+//   for (let cell of beverageCells) {
+//     if (cell.beverageCellColor === "white") {
+//       return cell.beverageCellColor;
+//     }
+//   }
+//   return cell.beverageCellColor;
+// }
 
 
 // function beverageColor(x) {
@@ -263,14 +437,6 @@ function beverageColor(x) {
 //   rect(beverageX, beverageY, 40, 70);
 //   fill(160);
 //   rect(beverageX, beverageY, 40, 10);
-// }
-
-
-// function mousePressed() {
-//   if (mouseX > beverageX && mouseX < beverageX + w && mouseY > beverageY && mouseY < beverageY + h) {
-//     beverageX = mouseX - w/2;
-//     beverageY = mouseY - h/2;
-//   }
 // }
 
 
