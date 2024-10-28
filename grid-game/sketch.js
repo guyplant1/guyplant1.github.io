7a6880c61d00505c9be0ac0748f9827dd2396da5
guyplant1 <guyplant1@hotmail.com>
2024-10-28 11:09:25 -0600

    Started working on the ideas of the grid-based game that I want to make. For now I'm just displaying the game board with random(255) using r, g, and b variables to display random colors for each cell in the grid, but I want to make it only maybe up to 10 different colors to have displayed, and using millis to display a new round started if a player wins a round, displaying a new and different colored grid.

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,15 +1,85 @@
-// Project Title
-// Your Name
-// Date
+// Grid-Based Game
+// Syhon Rylott
+// Oct 28th, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const CELL_SIZE = 25;
+let grid;
+let cols;
+let rows;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cols = Math.floor(width/CELL_SIZE);
+  rows = Math.floor(height/CELL_SIZE);
+  grid = generateRandomGrid(cols, rows);
 }
 
+
 function draw() {
+  noLoop();
   background(220);
+  displayGrid();
+  gameTimer();
 }
+
+
+//
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      // if (grid[y][x] === 0) {
+      //   fill("black");
+      // }
+      // else if (grid[y][x] === 1) {
+      //   fill("white");
+      // }
+
+      let r = random(255);
+      let g = random(255);
+      let b = random(255);
+
+      fill(r, g, b);
+
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+
+      // if (y < windowHeight/2) {
+      //   square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+      // }
+    }
+  }
+}
+
+
+//
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (x = 0; x < cols; x++) {
+      // toss in a 0 or 1 randomly
+      // if (random(100) < 50) {
+      //   newGrid[y].push(0);
+      // }
+      // else {
+      //   newGrid[y].push(1);
+      // }
+    }
+  }
+  return newGrid;
+}
+
+
+//
+function gameTimer() {
+  if (millis(5000)) {
+    displayGrid();
+  }
+}
+
+
+// Want to look at millis demo on p5js to see how to use millis to display a randomly colored grid.
+// Also want to split the game board in half with maybe the height.
